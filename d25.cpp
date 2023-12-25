a21a057d0b9ee46423690459aaa7db1e4a793443
stefdasca <stefdasca@gmail.com>
2023-12-25 13:21:55 +0200

    Day 25 solution improved

--- d25.cpp
+++ d25.cpp
@@ -9,71 +9,49 @@ map<string, int> set_sizes;
 
 void absorb_node(string absorbing, string new_parent)
 {
-	//prune any "self references"
 	vector<string> adj_from_absorbed_node = edges[absorbing];
 	for (int i = 0; i < adj_from_absorbed_node.size(); i++)
-	{
 		if (adj_from_absorbed_node[i] == new_parent)
 		{
 			adj_from_absorbed_node.erase(adj_from_absorbed_node.begin() + i);
 			i--;
 		}
-	}
-
-	//prune out references to the absorbed in the new parent adj list!
 	for (int i = 0; i < edges[new_parent].size(); i++)
-	{
 		if (edges[new_parent][i] == absorbing)
 		{
 			edges[new_parent].erase(edges[new_parent].begin() + i);
 			i--;
 		}
-	}
-
-	//put the adj list from the absorbed node into the new parent's adj list
 	for (string& s : adj_from_absorbed_node)
-	{
 		edges[new_parent].push_back(s);
-	}
 
 	edges.erase(absorbing);
 
 	for (auto it = edges.begin(); it != edges.end(); it++)
-	{
 		for (string& s : it->second)
-		{
 			if (s == absorbing)
-			{
 				s = new_parent;
-			}
-		}
-	}
 
 	for (int i = 0; i < vertices.size(); i++)
-	{
 		if (vertices[i] == absorbing)
 		{
 			vertices.erase(vertices.begin() + i);
 			i--;
 		}
-	}
-
 	set_sizes[new_parent]+= set_sizes[absorbing] + 1;
 }
 
 bool kargers()
 {
-	//karger's!
 	const int initial_V = edges.size();
 	int V = initial_V;
 	while (V > 2)
 	{
-		//pick random edge: random node and a random edge on its adj list
 		int n, i;
 		n = rand() % vertices.size();
 		i = rand() % edges[vertices[n]].size();
-
-		if (vertices[n] == edges[vertices[n]][i]) continue; //same subset, dont bother
+		if (vertices[n] == edges[vertices[n]][i]) 
+            continue;
 
 		absorb_node(edges[vertices[n]][i], vertices[n]);
 		V--;
@@ -88,11 +66,9 @@ bool kargers()
 			cout << "\tSet size: " << set_sizes[it->first] + 1 << endl;
 			set_size_total *= set_sizes[it->first] + 1;
 		}
-
 		for (string& ed : it->second)
-		{
-			if (ed != it->first) cuts++;
-		}
+			if (ed != it->first) 
+                cuts++;
 	}
 
 	cout << "made " << cuts / 2 << " cuts!" << endl;
